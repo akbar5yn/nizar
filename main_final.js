6a58f079c2aeaae0d25a8cc0e324c8e381473740
akbar5yn <akbar2000016129@webmail.uad.ac.id>
2022-06-06 07:55:22 +0700

    repair

--- main_final.js
+++ main_final.js
@@ -1,42 +1,35 @@
 Vue.createApp({
-    data() {
-      return {
-        message: "Hello Vue!",
-        header: {},
-        articles: []
-      };
+  data() {
+    return {
+      message: 'Hello Vue!',
+      header: {},
+      articles: [],
+    };
+  },
+  methods: {
+    getHeaderData() {
+      axios
+        .get('https://raw.githubusercontent.com/blinxyt23/tekweb/main/header.json')
+        .then((res) => {
+          this.header = res.data;
+          this.getArticles();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods:{    
-        getHeaderData()
-        {
-          axios
-            .get(
-              "https://raw.githubusercontent.com/blinxyt23/tekweb/main/header.json"
-            )
-            .then((res) => {              
-              this.header = res.data;
-              this.getArticles();
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        },
-        getArticles()
-        {
-            axios
-            .get(
-              "https://raw.githubusercontent.com/blinxyt23/tekweb/main/contents/articles.json"
-            )
-            .then((res) => {              
-              this.articles = res.data;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }
+    getArticles() {
+      axios
+        .get('https://raw.githubusercontent.com/akbar5yn/nizar/main/contents/articles.json')
+        .then((res) => {
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    beforeMount() {
-        this.getHeaderData()
-    }
-  }).mount("#app");
-  
+  },
+  beforeMount() {
+    this.getHeaderData();
+  },
+}).mount('#app');
--- read.js
+++ read.js
@@ -1,33 +1,29 @@
 Vue.createApp({
-    data() {
-      return {       
-        article: null,
-      };
+  data() {
+    return {
+      article: null,
+    };
+  },
+  methods: {
+    getMarkdownData() {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const article = urlParams.get('article');
+      var converter = new showdown.Converter();
+      console.log(article);
+      axios
+        .get('https://raw.githubusercontent.com/akbar5yn/nizar/main/contents/' + article)
+        .then((res) => {
+          var html = converter.makeHtml(res.data);
+          this.article = html;
+          console.log(html);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-      getMarkdownData()
-      {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const article = urlParams.get('article');        
-        var converter = new showdown.Converter();
-        console.log(article);
-        axios
-          .get(
-            "https://raw.githubusercontent.com/blinxyt23/tekweb/main/contents/"+article
-          )
-          .then((res) => {
-            var html = converter.makeHtml(res.data);           
-            this.article = html;
-            console.log(html);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    },
-    beforeMount() {
-      this.getMarkdownData();
-    },
-  }).mount("#app");
-  
+  },
+  beforeMount() {
+    this.getMarkdownData();
+  },
+}).mount('#app');
